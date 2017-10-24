447d4f57e2221ca7c0a24ea6b433e60e8936f46d
Alexandre Camus <alex.camus@hotmail.fr>
2017-10-24 11:29:15 +0200

    Add login option in argument
    You can optionally path username, password and databasename.
    If none is specified or at least one is missing, switching automatically to no auth connection.

--- src/main/java/fr/polux/Main.java
+++ src/main/java/fr/polux/Main.java
@@ -1,18 +1,8 @@
 package fr.polux;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.bulk.BulkWriteResult;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.*;
-import org.bson.Document;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -20,21 +10,48 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.bulk.BulkWriteResult;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.BulkWriteOptions;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOneModel;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.WriteModel;
+import org.bson.Document;
+
+import static java.util.Collections.singletonList;
+import static java.util.Optional.ofNullable;
+
 public class Main {
 
     /**
      * Need Mongo in standalone build or docker.
      * docker run -d -p 27017:27017 mongo:3.4.9
+     *
+     * Login arguments: username, password, databasename
      */
     public static void main(String[] args) throws UnknownHostException {
-        //MongoCredential credential = MongoCredential.createCredential("admin", "admin", "test".toCharArray());
+        MongoCredential credential = null;
+        try {
+            String userName = args[0];
+            String password = args[1];
+            String databaseName = args[2];
+            credential = MongoCredential.createCredential(userName, databaseName, password.toCharArray());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println ("no login passed, or missing one argument, switching to no auth connection");
+        }
         ServerAddress address = new ServerAddress(InetAddress.getLocalHost(), 27017);
 
         // without this timeout, it takes 30 seconds to get the exception...
         MongoClientOptions options = MongoClientOptions.builder().serverSelectionTimeout(5000).build();
 
-        MongoClient mongoClient = new MongoClient(address);
-        // MongoClient mongoClient = new MongoClient(address, Collections.singletonList(credential), options);
+        MongoClient mongoClient = ofNullable(credential)
+          .map(c -> new MongoClient(address, singletonList(c), options))
+          .orElse(new MongoClient(address));
 
         MongoCollection<Document> collection = mongoClient.getDatabase("test").getCollection("testcol");
 
